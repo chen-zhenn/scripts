ec0f4365aad3789f2858cb6ea2d697ea15117642
Carlos Garcia <chenzhen.dev@gmail.com>
2022-12-31 09:25:49 -0300

    Refactoring...

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,49 +2,48 @@
 
 date=$(date +"%d-%m-%G")
 user_id=$(cat /etc/passwd | grep $USER | cut -d : -f 3)
-source=$1
+backup_source=$1
 partition_type="exfat"
 partition_name="/dev/nvme0n1p7"
 partition_mount_point=$2
-backup_file_name="backup-linux-$( echo $source | tr -d /)-$date"
+backup_file_name="backup$( echo $backup_source | tr / "-")-$date"
+backup_local_path=0
 backup_remote_path=$3
 has_mounted=$(mount | grep -o $partition_mount_point)
 logs=logs.json
 
 mount_partition() {
-    local log_op_main=$(jq '.partition.op.main' $logs) | tr -d '"'
-    echo $log_op_main
-    sleep 5
-    mount -t $partition_type auto -o uid=$user_id $partition_name $partition_mount_point
+    local log_op_init=$(jq '.partition.op.main' $logs)
+    echo $log_op_init | tr -d '"'
+    sudo mount -t $partition_type -o uid=$user_id $partition_name $partition_mount_point
 }
 
 pack_dir_home() {
-    local src=$( echo $source | tr -d /)
-    local log_op_main=$(jq '.packing.op.main' $logs)
+    local log_op_init=$(jq '.packing.op.main' $logs)
     local log_op_sub=$(jq '.packing.op.sub' $logs) 
-    echo $log_op_main | tr -d '"'
+    echo $log_op_init
     echo "$log_op_sub" | tr -d '"'
-    tar -czf - -P $source | pv -p --timer --rate --bytes > "$backup_file_name.gz"
+    tar -czf - -P $backup_source | pv -p --timer --rate --bytes > "$backup_file_name.gz"
 }
 
 do_local_backup() {
-    local src=$( echo $source | tr -d /)
-    local log_op_main=$(jq '.backup.local.op.main' $logs)
-    local log_op_sub=$(jq '.backup.local.op.sub' $logs)
-    echo $log_op_main | tr -d '"'
-    echo "$log_op_sub $partition_mount_point..." | tr -d '"'
-    pv "$backup_file_name.gz" > "$partition_mount_point/$backup_file_name.gz"
+    [[ $backup_local_path -eq 0 ]] && backup_local_path=$partition_mount_point
+    local log_op_init=$(jq '.backup.local.op.main' $logs)
+    local log_op_sub=$(jq '.backup.local.op.sub' $logs)    
+    echo $log_op_init | tr -d '"'
+    echo "$log_op_sub $backup_local_path..." | tr -d '"'
+    pv "$backup_file_name.gz" > "$backup_local_path/$backup_file_name.gz"
 }
 
-do_remote_backup() {
-    local backup_file=$backup_file_name
-    local source="$partition_mount_point/$backup_file.gz"
-    local dest=$backup_remote_path   
-    local log_op_main=$(jq '.backup.remote.op.main' $logs)
-    local log_op_sub=$(jq '.backup.remote.op.sub' $logs)
-    echo $log_op_main | tr -d '"'
-    echo "$log_op_sub Google Drive em $dest..." | tr -d '"'
-    rclone copy -P $source gdrive:$dest
+do_remote_backup() {    
+    local source="$backup_local_path/$backup_file_name.gz"
+    local dest=$backup_remote_path
+    local log_op_init=$(jq '.backup.remote.op.main' $logs)
+    local log_op_sub=$(jq '.backup.remote.op.sub' $logs) 
+    echo $log_op_init | tr -d '"'
+    echo "$log_op_sub Google Drive em $backup_local_path..." | tr -d '"'
+    rclone mkdir gdrive:$dest
+    rclone copy -P $source gdrive:$dest    
 }
 
 logger() {
@@ -56,25 +55,84 @@ logger() {
     esac
 }
 
-main() {
+init() {
     pack_dir_home
     [[ $? -eq 0 ]] && logger 2
-    do_local_backup 
+    do_local_backup
     [[ $? -eq 0 ]] && logger 3
-    do_remote_backup 
+    do_remote_backup
     [[ $? -eq 0 ]] && logger 4
 }
 
-if [[ $has_mounted ]];then
-    echo $(jq '.partition.successed' $logs) | tr -d '"'
-    main
-    [[ $? -eq 0 ]] && cd ~
-    exit 0
-else
-    echo $(jq '.partition.fail' $logs) | tr -d '"'
-    mount_partition
-    [[ $? -eq 0 ]] && logger 1
-    main
-    [[ $? -eq 0 ]] && cd ~
-    exit 0
-fi
+main(){
+
+    abort_backup_task(){
+        echo "Deseja cancelar tarefa de backup? (s)Sim,(n)Não"
+        read confirm
+
+        if [[ $confirm == "s" ]];then
+            echo "Backup cancelado!"
+            exit 0
+            else main
+        fi 
+        echo "Backup cancelado!"
+        exit 0
+    }
+
+    create_backup_dir(){
+        echo 'Deseja fazer backup em outro diretório? (s)Sim,(n)Não'
+        read confirm
+
+        if [[ $confirm == "s" ]];then
+            echo 'Infome caminho do diretório! Ex: /caminho/meu-backup'
+            read directory_path
+
+            if [[ -d $directory_path ]];then
+                do_local_backup $directory_path
+                else
+                    echo -e "$directory_path não existe!\nDeseja cria-lo? (s)Sim,(n)Não"
+                    read confirm
+
+                    if [[ $confirm == "s" ]];then
+                        local user_dir=$HOME
+                        backup_local_path="$user_dir/BACKUP/$directory_path"
+                        mkdir -p $backup_local_path
+                        
+                        if [[ $? -eq 0 ]];then
+                            echo "Diretório criado em $backup_local_path"
+                            init
+                            [[ $? -eq 0 ]] && exit 0
+                            else
+                                echo "Não foi possivel criar diretório!"
+                                abort_backup_task
+                        fi
+                        else abort_backup_task
+                    fi
+            fi
+            else abort_backup_task
+        fi
+    }
+
+    if [[ -d $partition_mount_point ]];then
+
+        if [[ $has_mounted ]];then
+            echo $(jq '.partition.successed' $logs) | tr -d '"'
+            init
+            [[ $? -eq 0 ]] && exit 0
+        else
+            echo $(jq '.partition.fail' $logs) | tr -d '"'
+            echo 'Deseja montar partição para backup? (s)Sim,(n)Não'
+            read confirm
+            
+            if [[ $confirm == "s" ]];then
+                mount_partition
+                [[ $? -eq 0 ]] && logger 1
+                init
+                [[ $? -eq 0 ]] && exit 0
+            
+            else create_backup_dir
+            fi
+        fi
+    fi
+}
+main
